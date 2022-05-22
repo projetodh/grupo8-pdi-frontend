4fc9656a531aac942fc6f89c7dc00366e0fe959b
juvigh <juliolk.4321@gmail.com>
2022-05-22 18:22:07 -0300

    last version

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+var prevListCursos = [
+    {
+        id: 1,
+        nome: 'JavaScript',
+        descricao: ' Consiste em uma das linguagens de programação mais populares do mundo',
+        imgUrl: "assets/img/javascript1.png"
+    },
+
+    {
+        id: 1,
+        nome: 'JavaScript',
+        descricao: ' Consiste em uma das linguagens de programação mais populares do mundo',
+        imgUrl: "assets/img/javascript1.png"
+    },
+
+    {
+        id: 1,
+        nome: 'JavaScript',
+        descricao: ' Consiste em uma das linguagens de programação mais populares do mundo',
+        imgUrl: "assets/img/javascript1.png"
+    },
+
+    {
+        id: 1,
+        nome: 'JavaScript',
+        descricao: ' Consiste em uma das linguagens de programação mais populares do mundo',
+        imgUrl: "assets/img/javascript1.png"
+    }
+
+];
+
