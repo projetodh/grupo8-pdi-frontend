a5650fbe3fbae38d6f8afdc9836f796082f63cf3
juvigh <juliolk.4321@gmail.com>
2022-06-07 13:36:24 -0300

    js in cadastro form

---
+++ assets/js/cadastro.js
@@ -0,0 +1,104 @@
+console.log('JavaScript Carregado')
+
+const form = document.getElementById('form');
+const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const gender = document.getElementById('gender')
+const password = document.getElementById('password')
+const button = document.getElementById('check')
+
+ form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    validationInputs()
+    console.log('clicou')
+});
+
+// amanha eu preciso resolver esse problema de execução da funcao submitForm
+// resolver o problema da class control error e success (porque elas nao estao chamando)
+// preciso validar outros campos
+
+
+function validationInputs() {
+
+//    new Number(value)
+   const nomeValue = nome.value.trim()
+   const emailValue = email.value.trim()
+   const genderValue = gender.value.trim()
+   const passwordValue = password.value.trim()
+    
+    if(nomeValue === '') {
+        alertError(nome, 'Seu nome é obrigatório');
+    } else {
+        alertSuccess(nome);
+    }
+
+    if(emailValue === ''){
+        alertError(email, 'Seu email é obrigatório');
+    } else {
+        alertSuccess(email)
+    }
+
+    if(genderValue === '') {
+        alertError(gender, 'Seu gênero é obrigatório');
+
+    } else {
+        alertSuccess(gender)
+    }
+
+    if (passwordValue === '') {
+        alertError(password, 'Sua senha é obrigatória')
+    } else if(passwordValue.length < 8) {
+        alertError(password, 'Sua senha precisa de no mínimo 8 caracteres')
+
+    } else {
+        alertSuccess(password)
+    }
+};
+
+function inputName(){
+
+}
+
+function inputEmail(){
+
+}
+
+function inputGender(){
+
+}
+
+function inputPassword(){
+
+}
+
+
+
+function alertError(input, message) {
+    const control = input.parentElement;
+    const small = control.querySelector('small');
+
+    // Adc mensagem de erro
+    small.innerText = message;
+
+    // acionar a class de erro
+    control.className = 'form-container error'
+
+}
+
+function alertSuccess(input) {
+    const control = input.parentElement;
+    control.className = 'form-container success'
+
+//     const formContainer = form.querySelectorAll('form-container')
+
+// // for(let i = 0; i < formContainer.length; i++) {
+// //    () => return (control.className = 'form-container success')
+// // }
+
+// // const success = control.className = 'form-container success'
+
+// if(formContainer === success) {
+//     console.log('muito que bem')
+// }
+
+}
