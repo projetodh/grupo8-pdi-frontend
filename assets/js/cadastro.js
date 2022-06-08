22fc1306d6766d983b5dbdba3a751378091f5519
juvigh <juliolk.4321@gmail.com>
2022-06-08 12:35:00 -0300

    js in cadastro pg 3/5

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -7,16 +7,56 @@ const gender = document.getElementById('gender')
 const password = document.getElementById('password')
 const button = document.getElementById('check')
 
+
+
  form.addEventListener('submit', (event) => {
     event.preventDefault()
     validationInputs()
     console.log('clicou')
 });
 
-// amanha eu preciso resolver esse problema de execução da funcao submitForm
-// resolver o problema da class control error e success (porque elas nao estao chamando)
-// preciso validar outros campos
+nome.addEventListener('keyup', function(){
+    if(nome.value.trim() === "") {
+        alertError(nome, 'Seu nome é obrigatório')
+    } else if (nome.value.length < 10) {
+        alertError(nome, 'Digite um nome válido')
+    }
+    else {
+        alertSuccess(nome)
+    }
+    console.log('caracter: ' + nome.value)
+});
 
+email.addEventListener('keyup', function(){
+    if(email.value.trim() === '') {
+        alertError(email, 'Seu email é obrigatório')
+    }
+    else {
+        alertSuccess(email)
+    }
+});
+
+gender.addEventListener('keyup', function(){
+    if(gender.value.trim() === '') {
+        alertError(gender, 'Seu gênero é obrigatório')
+    } else {
+        alertSuccess(gender)
+    }
+});
+
+password.addEventListener('keyup', function(){
+    if(password.value.trim() === '') {
+        alertError(password, 'Sua senha é obrigatória')
+    } else if (password.value.length < 8) {
+        alertError(password, 'Sua senha deve conter no mínimo 8 caracteres')
+    } else {
+        alertSuccess(password)
+    }
+
+})
+
+// agora só falta arrumar o codigo de forma organizada e otimizada. Também criar uma função que gere uma ação depois
+// do submit no form após os inputs obedecerem todas as validações
 
 function validationInputs() {
 
@@ -48,7 +88,7 @@ function validationInputs() {
     if (passwordValue === '') {
         alertError(password, 'Sua senha é obrigatória')
     } else if(passwordValue.length < 8) {
-        alertError(password, 'Sua senha precisa de no mínimo 8 caracteres')
+        alertError(password, 'Sua senha deve conter no mínimo 8 caracteres')
 
     } else {
         alertSuccess(password)
